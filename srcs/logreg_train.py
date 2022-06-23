bb9ef85f62264a84a2ce39b9f75771eca8b1082a
Giulia Di lorenzo <gdi-lore@e4r9p4.42roma.it>
2022-06-23 15:02:57 +0200

    sistemazione

--- srcs/logreg_train.py
+++ srcs/logreg_train.py
@@ -1,7 +1,8 @@
 import sys
 import numpy as np
 import math
-from utils.read_csv import read_csv
+from utils.read_csv import read_csv_to_list
+# from sklearn.metrics import accuracy_score
 
 def hThetafunc(thetas, x):
 	temp = 0
@@ -17,22 +18,33 @@ def hThetafunc(thetas, x):
 			ret = 0
 	return ret
 
-def sumPartialDer(thetas, matrix, y, labels, j):
+# def sumPartialDer(thetas, matrix, y, labels, j):
+# 	ret = 0
+# 	for i in range(len(matrix[labels[j + 1]])):
+# 		if ~np.isnan(matrix[labels[j + 1]][i]):
+# 			ret += (hThetafunc(thetas, matrix[i]) - y[i]) * matrix[labels[j + 1]][i]
+# 	return ret
+
+def sumPartialDer(thetas, matrix, y, j):
 	ret = 0
-	for i in range(len(matrix[labels[j + 1]])):
-		if ~np.isnan(matrix[labels[j + 1]][i]):
-			ret += (hThetafunc(thetas, matrix[i]) - y[i]) * matrix[labels[j + 1]][i]
+	for i in range(matrix.shape[0]):
+		if ~np.isnan(matrix[i][j + i]):
+			ret += (hThetafunc(thetas, matrix[i]) - y[i]) * matrix[i][j + 1]
 	return ret
 
 if len(sys.argv) != 2:
 	print("Usage: python3 logreg_train.py fileName")
 	sys.exit(0)
 
-matrix_temp = read_csv(sys.argv[1], ',')
-labels = matrix_temp.dtype.names
-matrix = matrix_temp[[labels[1], labels[6], labels[7], labels[8], labels[10], labels[11], labels[12], labels[13], labels[14], labels[15], labels[16], labels[17], labels[18]]]
-labels = matrix.dtype.names
-n = len(labels) - 1
+matrix_temp = read_csv_to_list(sys.argv[1], ',')
+# labels = matrix_temp.dtype.names
+# print(labels)
+# matrix = matrix_temp[[labels[1], labels[6], labels[7], labels[8], labels[10], labels[11], labels[12], labels[13], labels[14], labels[15], labels[16], labels[17], labels[18]]]
+matrix = matrix_temp[:, [1, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18]]
+
+# labels = matrix.dtype.names
+# n = len(labels) - 1
+n = matrix.shape[1] - 1
 step = 30
 lr = 1 / 100
 
@@ -58,37 +70,41 @@ hthetas = np.zeros(n)
 sthetas = np.zeros(n)
 
 tempThetas = np.zeros(n)
-nRow = len(matrix[labels[0]])
+nRow = matrix.shape[0]
 
-gryf = np.fromiter((x == "Gryffindor" for x in matrix["Hogwarts House"]), int)
-raven = np.fromiter((x == "Ravenclaw" for x in matrix["Hogwarts House"]), int)
-huff = np.fromiter((x == "Hufflepuff" for x in matrix["Hogwarts House"]), int)
-slyth = np.fromiter((x == "Slytherin" for x in matrix["Hogwarts House"]), int)
+gryf = np.fromiter((x == "Gryffindor" for x in matrix[:, 1]), int)
+raven = np.fromiter((x == "Ravenclaw" for x in matrix[:, 1]), int)
+huff = np.fromiter((x == "Hufflepuff" for x in matrix[:, 1]), int)
+slyth = np.fromiter((x == "Slytherin" for x in matrix[:, 1]), int)
 
 
 for i in range(step):
 	for i in range(len(gthetas)):
-		tempThetas[i] = gthetas[i] - lr * (1/nRow) * sumPartialDer(gthetas, matrix, gryf, labels, i)
+		tempThetas[i] = gthetas[i] - lr * (1/nRow) * sumPartialDer(gthetas, matrix, gryf, i)
 	gthetas = tempThetas
 print(gthetas)
 
 for i in range(step):
 	for i in range(len(rthetas)):
-		tempThetas[i] = rthetas[i] - lr * (1/nRow) * sumPartialDer(rthetas, matrix, raven, labels, i)
+		tempThetas[i] = rthetas[i] - lr * (1/nRow) * sumPartialDer(rthetas, matrix, raven, i)
 	rthetas = tempThetas
 print(rthetas)
 
 for i in range(step):
 	for i in range(len(hthetas)):
-		tempThetas[i] = hthetas[i] - lr * (1/nRow) * sumPartialDer(hthetas, matrix, huff, labels, i)
+		tempThetas[i] = hthetas[i] - lr * (1/nRow) * sumPartialDer(hthetas, matrix, huff, i)
 	hthetas = tempThetas
 print(hthetas)
 
 for i in range(step):
 	for i in range(len(sthetas)):
-		tempThetas[i] = sthetas[i] - lr * (1/nRow) * sumPartialDer(sthetas, matrix, slyth, labels, i)
+		tempThetas[i] = sthetas[i] - lr * (1/nRow) * sumPartialDer(sthetas, matrix, slyth, i)
 	sthetas = tempThetas
 print(sthetas)
+
+# y_pred = ...
+# y_true = ...
+# print("Accuracy score: ", accuracy_score(y_true, y_pred))
 # thetas[0] = 1/1600 sum (g(thetas * riga i-esima) - risultato iesimo)) * matrix[i][0]
 # thetas * riga i-esima = 0 -> g = 1/2
 # risultato iesimo 1/2 o -1/2 che moltiplicano gli x[i][0]
--- srcs/utils/read_csv.py
+++ srcs/utils/read_csv.py
@@ -36,3 +36,40 @@ def read_csv(pathfile, sep=','):
 		i += 1
 	ret = np.array(matrix, dtype=label_type)
 	return ret
+
+def read_csv_to_list(pathfile, sep=','):
+	with open(pathfile, 'r') as file:
+	# RIGA 1 labels
+		row = file.readline()
+		label_temp = []
+		label = []
+		temp = row.split(sep)
+		for x in temp:
+			label_temp.append((x.strip(), 'f4'))
+			label.append(x.strip())
+	# RIGA 2 e tutte le successive, creiamo una matrice di dati.
+		matrix = []
+		for r in file:
+			temp = r.split(sep)
+			temp2 = []
+			for x in temp:
+				try:
+					value = float(x)
+				except:
+					if (str(x) != ""):
+						value = str(x)
+					else:
+						value = np.nan
+				temp2.append(value)
+			matrix.append(temp2)
+	label_type = []
+	i = 0
+	for x in matrix[0]:
+		if isinstance(x, float):
+			label_type.append("<f4")
+		else:
+			label_type.append('<U20')
+		i += 1
+	ret = np.array(matrix)
+	# , dtype={'names': label, 'formats': label_type})
+	return ret
